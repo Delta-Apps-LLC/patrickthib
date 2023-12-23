eba452a41ee4847aad01d22b331c2a2092a65fd9
athibau2 <a.thibs98@gmail.com>
2023-12-23 16:34:48 -0700

    added iframe for ted talk

--- pages/index.vue
+++ pages/index.vue
@@ -10,17 +10,50 @@
     <PictureCard :items="introPictureCards" />
 
     <div class="centered">
-      <span class="header main-white text-center"
-        style="font-size: 25px !important; padding: 50px 0 0 0 !important;"
+      <span class="small-header main-white text-center"
         data-aos="zoom-in"
         data-aos-duration="1000"
       >
         {{ awardsTitle }}
       </span>
-      <v-divider />
-      <p>LEED FELLOW</p>
-      <p>LFA</p>
-      <p>CCS</p>
+
+      <v-expansion-panels class="expand-group main-white text-font"
+        data-aos="fade-in"
+        data-aos-duration="2500"
+        style="margin-bottom: 40px;"
+      >
+        <v-expansion-panel
+          v-for="(item, i) in awardsAchievements"
+          :key="i"
+        >
+          <v-expansion-panel-header>
+            <span style="width: 90%;">{{ item.title }}</span>
+            <v-spacer />
+            <span>{{ item.year }}</span>
+          </v-expansion-panel-header>
+          <v-expansion-panel-content>{{ item.details }}</v-expansion-panel-content>
+        </v-expansion-panel>
+      </v-expansion-panels>
+    </div>
+
+    <div class="centered">
+      <span class="small-header main-white text-center"
+        data-aos="zoom-in"
+        data-aos-duration="1000"
+      >
+        Recent Highlights
+      </span>
+      <iframe style="margin: 0 auto 40px auto;"
+        data-aos="fade-in"
+        data-aos-duration="2500"
+        :width="windowWidth < 650 ? '90%' : windowWidth < 900 ? '70%' : '60%'"
+        :height="windowWidth < 650 ? '240' : '340'"
+        :src="tedTalkURL"
+        title="YouTube video player"
+        frameborder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        allowfullscreen
+      ></iframe>
     </div>
 
   </div>
@@ -52,13 +85,47 @@ export default {
           text: 'Hi, I\'m Patrick Thibaudeau. As a dedicated architect with a passion for sustainability and green design, my journey in the field has been shaped by a steadfast commitment to creating spaces that not only meet the needs of the present but also embrace a responsible and environmentally conscious future.\n\nThroughout my entire career, I have consistently championed the principles of sustainability, recognizing the crucial role architecture plays in mitigating environmental impact. From incorporating energy-efficient technologies to advocating for eco-friendly materials, every project I undertake reflects a deep-seated belief in the transformative power of green design.\n\nEngaging in the relentless pursuit of sustainable practices has not only defined my professional identity but has become a personal mission, as I strive to contribute meaningfully to the collective efforts in building a harmonious coexistence between architecture and the environment.',
           image: 'images/profile.jpg',
         },
-        {
-          text: 'Hi, I\'m Patrick Thibaudeau. As a dedicated architect with a passion for sustainability and green design, my journey in the field has been shaped by a steadfast commitment to creating spaces that not only meet the needs of the present but also embrace a responsible and environmentally conscious future.\n\nThroughout my entire career, I have consistently championed the principles of sustainability, recognizing the crucial role architecture plays in mitigating environmental impact. From incorporating energy-efficient technologies to advocating for eco-friendly materials, every project I undertake reflects a deep-seated belief in the transformative power of green design.\n\nEngaging in the relentless pursuit of sustainable practices has not only defined my professional identity but has become a personal mission, as I strive to contribute meaningfully to the collective efforts in building a harmonious coexistence between architecture and the environment.',
-          image: 'images/profile.jpg',
-        },
       ],
       tagline: 'Sustainable Spaces, Endless Possibilities',
       awardsTitle: 'Awards and Achievements',
+      awardsAchievements: [
+        {
+          title: 'Environmental Leader 75',
+          details: 'The annual Environmental Leader 75 is a list of the top 75 executives in environmental and sustainability management who are “driving their companies, and the industry itself, forward and are making a difference,” as selected by the Editorial and Management team at Business Sector Media.',
+          year: 2018,
+        },
+        {
+          title: 'LA Harbor College Outstanding Project of the Year, Energy and Innovation Awards',
+          details: '',
+          year: 2016,
+        },
+        {
+          title: 'LA Harbor College SCUP Award',
+          details: '',
+          year: 2015,
+        },
+        {
+          title: 'CCFC Award of Excellence',
+          details: '',
+          year: 2014,
+        },
+        {
+          title: 'LEED FELLOW',
+          details: 'Green Business Certification Inc. (GBCI)',
+          year: 2014,
+        },
+        {
+          title: 'National Environmental Stewardship Award',
+          details: 'Firm award achieved under my leadership.',
+          year: 2007,
+        },
+        {
+          title: 'Rebecca L Foss Environmental Stewardship Award',
+          details: 'Firm award achieved under my leadership.',
+          year: 2006,
+        },
+      ],
+      tedTalkURL: 'https://www.youtube.com/embed/edmrxrdDePQ?si=JIS9tDKbKdcgxltg',
       windowWidth: window.innerWidth,
     }
   },
