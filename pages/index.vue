57e2856b33c5ec279caa4a30b5e4ae94bc9acdb2
athibau2 <a.thibs98@gmail.com>
2023-12-23 20:21:53 -0700

    fixing image paths

--- pages/index.vue
+++ pages/index.vue
@@ -140,7 +140,7 @@ export default {
       introPictureCards: [
         {
           text: 'Hi, I\'m Patrick Thibaudeau. As a dedicated architect with a passion for sustainability and green design, my journey in the field has been shaped by a steadfast commitment to creating spaces that not only meet the needs of the present but also embrace a responsible and environmentally conscious future.\n\nThroughout my entire career, I have consistently championed the principles of sustainability, recognizing the crucial role architecture plays in mitigating environmental impact. From incorporating energy-efficient technologies to advocating for eco-friendly materials, every project I undertake reflects a deep-seated belief in the transformative power of green design.\n\nEngaging in the relentless pursuit of sustainable practices has not only defined my professional identity but has become a personal mission, as I strive to contribute meaningfully to the collective efforts in building a harmonious coexistence between architecture and the environment.',
-          image: 'images/profile.jpg',
+          image: '/images/profile.jpg',
         },
       ],
       tagline: 'Sustainable Spaces, Endless Possibilities',
